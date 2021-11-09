990d825339bc20c8ac6ffa886b10d9f8561f694b
Wenyan Zhang <wenyanzhang@wenyans-air.win.ad.jhu.edu>
2021-11-09 15:42:19 -0500

    added comments, started working on cheecking property 2 & 3

--- GraphVerifier.py
+++ GraphVerifier.py
@@ -1,5 +1,6 @@
 import networkx as nx
 
+#checks the first property of Wheeler Graph
 #checks if the node with 0 in-degree comes first in the ordering
 def check_first_node(graph, order):
     all_nodes = nx.nodes(graph)
@@ -9,3 +10,20 @@ def check_first_node(graph, order):
         #find the node with 0 in-degree
         if graph.in_degree(node) == 0:
             return node == first_node
+
+#checks the second property of Wheeler Graph
+#returns true if for any pair of edges e = (u, v) and e' = (u', v')
+#labeled a and a' respectively, a < a' => v < b'
+#returns false otherwise
+def check_diff_edge(graph, order):
+    all_nodes = nx.nodes(graph)
+    return true
+   
+
+#checks the third property of Wheeler Graph
+#returns true if for any pair of edges e = (u, v) and e' = (u', v')
+#labeled a and a' respectively, (a = a') ^ (u < u') => v < v'
+#returns false otherwise
+def check_same_edge(graph, order):
+    all_nodes = nx.nodes(graph)   
+    return true

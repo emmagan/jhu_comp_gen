97a1a4147243b6b040aa4776c089da69cf2a120b
emmagan <egan1@jhu.edu>
2021-11-30 16:29:37 -0500

    Benchmark naive and partition approach, no vis

--- benchmark.py
+++ benchmark.py
@@ -1,12 +1,16 @@
 from timeit import timeit
 import os
+import pandas as pd
 from functools import partial
 from main import ordering, no_ordering
 
+data = {}
 path = 'data/manual'
 dirs = os.listdir( path )
 for fname in dirs:
     file = os.path.join(path, fname)
-
-print(timeit(partial(ordering, file, False, 'naive'), number=5)) # args are path, vis, approach
-print(timeit(partial(ordering, file, False, 'partition'), number=5)) # args are path, vis, approach
+    ntime = timeit(partial(ordering, file, False, 'naive'), number=10) # args are path, vis, approach
+    ptime = timeit(partial(ordering, file, False, 'partition'), number=10) # args are path, vis, approach
+    data[fname] = [ntime, ptime]
+df = pd.DataFrame.from_dict(data, orient='index', columns=['naive', 'partition'])
+print(df)
